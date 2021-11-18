70b4c3f2c4104cf9fc99dab58c39433929f4ffdd
sareli89 <sareli89@users.noreply.github.com>
2021-11-17 23:04:12 -0500

    Semana de fundamentos de la programacion - js

---
+++ fundamentos_programacion/app.js
@@ -0,0 +1,49 @@
+console.log('Hola desde js')
+
+/* Soy un comentario */
+
+let age = 0 //declarar y asignar un valor
+console.log('Edad', age)
+age = 25
+console.log('Edad', age)
+
+let firstName
+console.log('Nombre', firstName)
+firstName = 'Naomi'
+console.log('Nombre', firstName)
+
+/* Buena Practica */
+
+let lastName = ''
+console.log('Apellido', lastName)
+lastName = 'LopeZ Puertos' 
+console.log('Apellido', lastName)
+
+const pi = 3.1416
+
+// pi = 2.1416 --> no se le puede reasignar un valor 
+
+// Soy un comentario de linea
+
+var number1 = 10
+var number2 = '10'
+
+// == compara dos valores
+console.log(number1 == number2)
+
+// === compara valor y tipo de dato
+console.log(number1 === number2)
+
+// != compara el valor
+console.log(number1 != number2)
+
+// !== compara el valor y tipo de dato
+console.log(number1 !== number2)
+
+var myName = 'Sara' 
+var lastName1 = 'Reveles'
+var lastName2 = 'Caldera' 
+var myAge = 32
+var job = 'ama de casa y mamá'
+
+console.log("Hola, me llamo:", myName, lastName1, lastName2, "soy ", job, "y tengo ", myAge)
